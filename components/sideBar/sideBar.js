8894b13483f136e0cdd47c411b0f3627001be29f
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2021-08-19 00:32:43 +0530

    Modifes side bar

--- components/sideBar/sideBar.js
+++ components/sideBar/sideBar.js
@@ -1,37 +1,51 @@
 import React from "react";
-import styles from "./sideBar.module.css"
+import styles from "./sideBar.module.css";
 import Head from "../../util/head";
 
 export default class SideBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    
-    render() {
-        const {} = this.state;
-        return (
-            <div className={styles.container}>
-                <Head />
-                <img src="/assets/logo-icon.png" className={styles.logo} />
+	render() {
+		const {} = this.state;
+		return (
+			<div className={styles.container}>
+				<Head />
+				<img src="/assets/logo-icon.png" className={styles.logo} />
 
-                <div className={styles.sideBarOptions}>
-                <i className={`fa fa-home ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-television ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-cube ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-briefcase ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-users ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-shopping-bag ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-comment ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-envelope ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-file ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-desktop ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-check-square-o ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-bolt ${styles.icons}`} aria-hidden="true"></i>
-                <i className={`fa fa-list ${styles.icons}`} aria-hidden="true"></i>
-                </div>
-            </div>
-        )
-    }
+				<div className={styles.sideBarOptions}>
+					<i
+						className={`fa fa-home ${styles.icons}`}
+						aria-hidden="true"
+					/>
+					<i
+						className={`fa fa-television ${styles.icons}`}
+						aria-hidden="true"
+					/>
+					<i
+						className={`fa fa-cube ${styles.icons}`}
+						aria-hidden="true"
+					/>
+					<i
+						className={`fa fa-briefcase ${styles.icons}`}
+						aria-hidden="true"
+					/>
+					<i
+						className={`fa fa-users ${styles.icons}`}
+						aria-hidden="true"
+					/>
+					<i
+						className={`fa fa-shopping-bag ${styles.icons}`}
+						aria-hidden="true"
+					/>
+					<i
+						className={`fa fa-comment ${styles.icons}`}
+						aria-hidden="true"
+					/>
+				</div>
+			</div>
+		);
+	}
 }
