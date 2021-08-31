4198d3215468f4e8b1169cfef868b8d2e422e588
Keerthika <elangokeer@gmail.com>
2021-08-31 13:01:03 +0530

    add btn

--- pages/employee/index.js
+++ pages/employee/index.js
@@ -1,6 +1,5 @@
 import { Formik, Form } from "formik";
-import { Container, Flex } from "@chakra-ui/react";
-
+import { Container, Flex, Button } from "@chakra-ui/react";
 import styles from "../../styles/admin.module.css";
 
 import CustomInput from "../../components/customInput/customInput";
@@ -8,6 +7,7 @@ import Head from "../../util/head";
 import GlobalWrapper from "../../components/globalWrapper/globalWrapper";
 import { Validation } from "../../util/validation";
 import Table from "../../components/table/table";
+import Link from "next/link";
 
 function Registration() {
 	const initialValue = {
@@ -23,24 +23,18 @@ function Registration() {
 		joining_date: "Joining Date",
 		resignation_date: "Resignation Date",
 		status: "Status",
-		action: "Action",
 	};
 
 	const valuesNew = [
 		{
-			id: "1",
-			name: "Ciddarth",
-			store_name: "Store 1",
-			designation: "Manager",
-			joining_date: "21/08/22",
-			resignation_date: "22/08/22",
-			status: "Active",
+			id: <span>{"1"}</span>,
+			name: <span>{"Ciddarth"}</span>,
+			store_name: <span>{"Store 1"}</span>,
+			designation: <span>{"Manager"}</span>,
+			joining_date: <span>{"21/08/22"}</span>,
+			resignation_date: <span>{"22/08/22"}</span>,
+			status: <span>{"Active"}</span>,
 		},
-	];
-
-	const values = [
-		["1", "KEERTHI", "Saravana", "Manager", "12.10/1996", "12/10/2000", "Active", <img src={"/assets/edit.png"} className={styles.icon} />],
-		["2", "NAME 1", "Krishna Sweets", "Manager", "12.10/1996", "12/10/2000", "Active", <img src={"/assets/edit.png"} className={styles.icon} />],
 	];
 
 	const sortCallback = (key, type) => {
@@ -60,7 +54,16 @@ function Registration() {
 					<Head />
 					<Flex templateColumns="repeat(3, 1fr)" gap={6} colSpan={2}>
 						<Container className={styles.container} boxShadow="lg">
-							<p>Employee</p>
+							<p className={styles.buttoninputHolder} >
+								<div>Employee</div>
+								<div style={{paddingRight: 10}}>
+									<Button colorScheme="purple">
+										<Link href="/employee/create">
+											{"Add"}
+										</Link>
+									</Button>
+								</div>
+							</p>
 							<div>
 								<div className={styles.personalInputHolder}>
 									<CustomInput label="Store" name="stores" type="text" method="switch" />
