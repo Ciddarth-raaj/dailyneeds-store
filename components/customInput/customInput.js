ab2594450e87d3e86fa298babd1a680eedb15857
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2021-09-05 18:56:42 +0530

    Adds prefilled data

--- components/customInput/customInput.js
+++ components/customInput/customInput.js
@@ -9,85 +9,91 @@ const CustomDateTimeInput = forwardRef(({ value, onClick, onChange }, ref) => (
 	<Input value={value} onChange={onChange} autoComplete="off" ref={ref} onClick={onClick} />
 ));
 
-const TextField = ({ label, values, method, selected, onChange, containerStyle, ...props }) => {
+const TextField = ({ label, values, method, selected, onChange, containerStyle, editable, ...props }) => {
 	const { setFieldValue } = useFormikContext();
 	const [field, meta] = useField(props);
+	console.log(field);
 
 	return (
 		<div className={styles.personalInputs} style={containerStyle}>
-			<label htmlFor={field.name} className={styles.label}>
+			<label htmlFor={field.name} className={`${styles.label} ${styles.infoLabel}`}>
 				{label}
 			</label>
-			{method === "TextArea" && (
-				<Textarea
-					{...field}
-					{...props}
-					width="100%"
-					// height="73%"
-					size="lg"
-				/>
-			)}
-			{method === "switch" && (
-				<Select {...field} placeholder="Select Option">
-					{values?.map((m) => (
-						<Fragment>
-							<option value={m.id}>{m.value}</option>
-							<ErrorMessage
-								component="div"
-								name={field.name}
-								className={styles.errorMessage}
+			{
+				true ? <p className={styles.infoText}>{field.value}</p> : <>
+					{method === "TextArea" && (
+						<Textarea
+							{...field}
+							{...props}
+							width="100%"
+							// height="73%"
+							size="lg"
+						/>
+					)}
+					{method === "switch" && (
+						<Select {...field} placeholder="Select Option">
+							{values?.map((m) => (
+								<Fragment>
+									<option value={m.id}>{m.value}</option>
+									<ErrorMessage
+										component="div"
+										name={field.name}
+										className={styles.errorMessage}
+									/>
+								</Fragment>
+							))}
+						</Select>
+					)}
+					{method === "timepicker" && (
+						<>
+							<DatePicker
+								{...field}
+								showTimeSelect
+								showTimeSelectOnly
+								timeCaption="Time"
+								dateFormat="hh:mm:ss"
+								{...props}
+								selected={(field.value && new Date(field.value)) || null}
+								onChange={val => {
+									setFieldValue(field.name, val);
+								}}
+								customInput={<CustomDateTimeInput />}
 							/>
-						</Fragment>
-					))}
-				</Select>
-			)}
-			{method === "timepicker" && (
-				<>
-					<DatePicker
-						{...field}
-						showTimeSelect
-						showTimeSelectOnly
-						timeCaption="Time"
-						dateFormat="hh:mm:ss"
-						{...props}
-						selected={(field.value && new Date(field.value)) || null}
-						onChange={val => {
-							setFieldValue(field.name, val);
-						}}
-						customInput={<CustomDateTimeInput />}
-					/>
-					{selected === "" && (
-						<ErrorMessage
-							component="div"
-							name={field.name}
-							className={styles.errorMessage}
-						/>
+							{selected === "" && (
+								<ErrorMessage
+									component="div"
+									name={field.name}
+									className={styles.errorMessage}
+								/>
+							)}
+						</>
+					)}
+					{method === "datepicker" && (
+						<>
+							<DatePicker
+								{...field}
+								{...props}
+								selected={(field.value && new Date(field.value)) || null}
+								onChange={val => {
+									setFieldValue(field.name, val);
+								}}
+								customInput={<CustomDateTimeInput />}
+							/>
+							{selected === "" && (
+								<ErrorMessage
+									component="div"
+									name={field.name}
+									className={styles.errorMessage}
+								/>
+							)}
+						</>
+					)}
+					{method === undefined && (
+						<Input {...field} {...props} autoComplete="off" />
 					)}
 				</>
-			)}
-			{method === "datepicker" && (
-				<>
-					<DatePicker
-						{...field}
-						{...props}
-						selected={(field.value && new Date(field.value)) || null}
-						onChange={val => {
-							setFieldValue(field.name, val);
-						}}
-						customInput={<CustomDateTimeInput />}
-					/>
-					{selected === "" && (
-						<ErrorMessage
-							component="div"
-							name={field.name}
-							className={styles.errorMessage}
-						/>
-					)}
-				</>
-			)}
-			{method === undefined && (
-				<Input {...field} {...props} autoComplete="off" />
-			)}
+
+			}
 			<ErrorMessage
 				component="div"
 				name={field.name}
