b7ea0bdbcf063fb83b173555f5680ee554adb9ae
ciddarthjrj <121765398+ciddarthjrj@users.noreply.github.com>
2025-01-09 17:16:16 +0530

    adds backend url

--- constants/api.js
+++ constants/api.js
@@ -1,6 +1,4 @@
 export default {
-  // BASE_URL: "http://192.168.0.100:8080",
-  BASE_URL: "http://127.0.0.1:8080",
-  // BASE_URL: "http://192.168.1.5:8080",
-  //  BASE_URL: "http://13.234.144.72:8080",
+  BASE_URL: "http://3.109.76.230/api",
+  // BASE_URL: "http://127.0.0.1:8080",
 };
