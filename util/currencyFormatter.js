94d2bd8da60c4ff0913ddeb2cd0e1df8f3d49ebd
ciddarthjrj <121765398+ciddarthjrj@users.noreply.github.com>
2025-03-11 12:25:00 +0530

    fix: fix build error

--- util/currencyFormatter.js
+++ util/currencyFormatter.js
@@ -1,6 +1,6 @@
 export default function currencyFormatter(value, minFractionDigits = 0) {
   let formatter = new Intl.NumberFormat("en-IN", {
-    minimumFractionDigits: minFractionDigits,
+    minimumFractionDigits: 0,
   });
 
   return formatter.format(value);
