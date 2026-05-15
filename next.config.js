1e7dcd37f5251525dbac517e9c366f950d047082
Ciddarth Raaj J R J <ciddarthjeyakumar@gmail.com>
2026-05-15 10:24:17 +0530

    chore: remove deploy.sh script and update Next.js configuration to disable standalone output mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  output: "standalone",
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
